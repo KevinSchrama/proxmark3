ce2912757b769264262841a6163daf3af786a990
KevinSchrama <55480855+KevinSchrama@users.noreply.github.com>
2022-02-28 14:11:13 +0100

    Update my_script.lua

--- client/luascripts/my_script.lua
+++ client/luascripts/my_script.lua
@@ -3,14 +3,14 @@ local ansicolors  = require('ansicolors')
 
 local function main(args)
     local uid = 1
-    core.console('hf iclass sim -t 0 --csn E012FFF902B93314')
+    core.console('hf iclass sim -t 0 --csn 4B6576696EB93314')
     os.execute('sleep 1')
     for type = 1, 9, 1 do
 
         print(type)
         if type ~= 3 and type ~= 4 and type ~= 5 then
         -- send command to proxmark
-        core.console('hf 14a sim -t '..type..' -u 0000000000000'..uid)
+        core.console('hf 14a sim -t '..type..' -u 4B6576696E000'..uid)
         os.execute('sleep 1')
         uid = uid + 1
         end
