30da4cf84607f0a0d7d0ed778b42c3fd9d81ee9f
KevinSchrama <kevin.com@live.nl>
2022-03-03 11:52:33 +0100

    Added delays to test.lua

--- client/luascripts/test.lua
+++ client/luascripts/test.lua
@@ -18,28 +18,40 @@ local function main(args)
         core.console('lf simstop')
     elseif typecard == '1' then
         core.console('lf hid sim -r 3006ec0c86')                    --1006ec0c86
+        os.execute('sleep 2')
     elseif typecard == '2' then
         core.console('lf em 410x sim --id 0F0368568B')              --0F0368568B
+        os.execute('sleep 1.5')
     elseif typecard == '3' then
         core.console('lf awid sim --fmt 26 --fc 234 --cn 1337')     --6d40a73
+        os.execute('sleep 2')
     elseif typecard == '4' then
         core.console('lf noralsy sim --cn 1338')                    --fefe
+        os.execute('sleep 2')
     elseif typecard == '5' then
         core.console('lf paradox sim -r 0f55555695596a6a9999a59a')  --218277aacb
+        os.execute('sleep 1.5')
     elseif typecard == '6' then
         core.console('hf iclass sim -t 0 --csn 4B6576696EB93314')
+        os.execute('sleep 2')
     elseif typecard == '7' then
         core.console('hf 14a sim -t 1 -u 4B6576696E0007')
+        os.execute('sleep 1')
     elseif typecard == '8' then
         core.console('hf 14a sim -t 2 -u 4B6576696E0008')
+        os.execute('sleep 1')
     elseif typecard == '9' then
         core.console('hf 14a sim -t 6 -u 4B6576696E0009')
+        os.execute('sleep 1')
     elseif typecard == '10' then
         core.console('hf 14a sim -t 7 -u 4B6576696E000A')
+        os.execute('sleep 1')
     elseif typecard == '11' then
         core.console('hf 14a sim -t 8 -u 4B6576696E000B')
+        os.execute('sleep 1')
     elseif typecard == '12' then
         core.console('hf 14a sim -t 9 -u 4B6576696E000C')
+        os.execute('sleep 1')
     end
 
 end
